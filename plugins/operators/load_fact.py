b025888dd0b086967464d28ca38c798295e87a4b
Gonzalo Gomez Millan del Villar <gonzalogomezmillandelvillar@gmail.com>
2023-01-30 20:38:13 +0100

    Update load_fact.py

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -1,6 +1,8 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
 
 class LoadFactOperator(BaseOperator):
 
@@ -11,37 +13,37 @@ class LoadFactOperator(BaseOperator):
         {}
         ACCESS_KEY_ID '{}'
         SECRET_ACCESS_KEY '{}'
-        IGNOREHEADER {}
+        IGNOREHEADER {} 
         DELIMITER '{}'
     """
 
     @apply_defaults
     def __init__(self,
                  redshift_conn_id = "",
-                 aws_credentials = "",
+                 aws_credentials_id = "",
                  sql_query = "",
-                 ignore_headers = 1,
                  delimiter = ",",
+                 ignore_headers = 1,
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
-        self.aws_credentials = aws_credentials
+        self.aws_credentials_id = aws_credentials_id
         self.sql_query = sql_query
-        self.ignore_headers = ignore_headers
         self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
 
     def execute(self, context):
-        aws_hook = AwsHook(self.aws_credentials)
+        aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
         
-        self.log.info("Upserting data into the fact table...")
-        formatted_sql = LoadFactOperator.copy_sql.format(
+        self.log.info("Upserting data to Redshift")
+        formatted_sql = LoadFactOperator.load_sql.format(
             self.sql_query,
             credentials.access_key,
             credentials.secret_access_key,
-            self.ignore_header,
+            self.ignore_headers,
             self.delimiter
         )
         
