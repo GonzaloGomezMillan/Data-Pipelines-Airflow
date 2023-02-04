11186f042c84b2f22868ed1bdcaae60965fbb0d7
Gonzalo Gomez Millan del Villar <gonzalogomezmillandelvillar@gmail.com>
2023-02-04 08:20:14 +0100

    Update load_dimension.py

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -10,41 +10,26 @@ class LoadDimensionOperator(BaseOperator):
     
     load_sql = """
         {}
-        ACCESS_KEY_ID = '{}'
-        SECRET_ACCESS_KEY = '{}'
-        IGNOREHEADER {}
-        DELIMITER '{}'
-        """
+        {}
+    """
     
 
     @apply_defaults
     def __init__(self,
                  redshift_conn_id = "",
-                 aws_credentials_id = "",
+                 table = "",
                  sql_query = "",
-                 delimiter = ",",
-                 ignore_headers = 1,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
-        self.aws_credentials_id = aws_credentials_id
+        self.table = table
         self.sql_query = sql_query
-        self.delimiter = delimiter
-        self.ignore_headers = ignore_headers
-
+        
     def execute(self, context):
-        aws_hook = AwsHook(self.aws_credentials)
-        credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
         
         self.log.info("Upserting data to Redshift")
-        formatted_sql = LoadDimensionOperator.load_sql.format(
-            self.sql_query,
-            credentials.access_key,
-            credentials.secret_access_key,
-            self.ignore_headers,
-            self.delimiter
-        )
+        formatted_sql = LoadDimensionOperator.load_sql.format(self.table, self.sql_query)
         
         redshift.run(formatted_sql)
