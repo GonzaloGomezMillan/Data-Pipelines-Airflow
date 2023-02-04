c1f29f7a0771080c913465c6ea199c7f4e5153ef
Gonzalo Gomez Millan del Villar <gonzalogomezmillandelvillar@gmail.com>
2023-02-04 08:20:12 +0100

    Update data_quality.py

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -10,19 +10,20 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id = "",
-                 table = "",
+                 tables = "",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.table = table
+        self.tables = tables
         self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        records = redshift_hook.get_records("SELECT COUNT(*) FROM {self.table}")
-        if len(records) < 1 or len(records[0]) < 1:
-                                           raise ValueError(f"Data quality check failed. {self.table} returned no results.")
-        num_records = records[0][0]
-        if num_records < 1:
-                                           raise ValueError(f"Data quality check failed. {self.table} contained 0 rows.")
-        logging.info(f"Data quality on table {self.table} check passed with {records[0][0]} records")
+        for table in self.tables:
+            records = redshift_hook.get_records("SELECT COUNT(*) FROM {}".format(table))
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError("Data quality check failed. {} returned no results.".format(table))
+            num_records = records[0][0]
+            if num_records < 1:
+                raise ValueError("Data quality check failed. {} contained 0 rows.".format(table))
+            self.log.info("Data quality on table {} check passed with {} records".format(table, num_records))
