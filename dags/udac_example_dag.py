d7096e500ac8e1d2b89e2aefe830c9fce9e9bc88
Gonzalo Gomez Millan del Villar <gonzalogomezmillandelvillar@gmail.com>
2023-02-04 15:18:12 +0100

    Update udac_example_dag.py

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -212,6 +212,3 @@ load_user_dimension_table >> run_quality_checks
 load_artist_dimension_table >> run_quality_checks
 load_time_dimension_table >> run_quality_checks
 run_quality_checks >> end_operator
-
-
-
